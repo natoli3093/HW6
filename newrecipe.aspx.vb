100757b9f207e645269902b7c0cac672dc6ab124
Kyle Natoli <kyle-a-natoli@uiowa.edu>
2014-10-26 16:45:07 -0500

    added new recipe web page to insert entries

---
+++ newrecipe.aspx.vb
@@ -0,0 +1,10 @@
+﻿
+Partial Class details
+    Inherits System.Web.UI.Page
+
+    Protected Sub DetailsView1_ItemInserted(sender As Object, e As DetailsViewInsertedEventArgs) Handles DetailsView1.ItemInserted
+
+        Response.Redirect("~/Default.aspx")
+
+    End Sub
+End Class
